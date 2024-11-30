e87292a2a4282df07ff3512b3c9241cd1c386c3e
Holden <holden.madagame@gmail.com>
2024-11-30 09:24:53 +0100

    Added styling and text

---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("fonts");
+  // eleventyConfig.addPassthroughCopy(
+  //   "./fonts/Libre_Franklin/LibreFranklin-Italic-VariableFont_wght.ttf"
+  // );
+  // eleventyConfig.addPassthroughCopy("./fonts/Hind/Hind-Medium.ttf");
+  eleventyConfig.addPassthroughCopy("img");
+}
